c9e9171aa1e52b75eaf1f9c5e24b56e0e77cfa2a
Gedichter <shiyuhuang@g.harvard.edu>
2018-04-25 11:26:24 -0400

    Initial Commit

---
+++ Parallel_LSM_Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Parallel_LSM_Tree
+//
+//  Created by Shiyu Huang on 4/25/18.
+//  Copyright © 2018 Shiyu Huang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
